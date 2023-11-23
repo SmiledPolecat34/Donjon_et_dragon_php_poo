f44990c1a9ff7c28fe3b8abdafe19ae8e2bd2398
SmiledPolecat34 <versayo03@gmail.com>
2023-11-23 10:35:55 +0100

    Retour

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+class Personnage{
+    private $nom;
+    private $pointsDeVie;
+    private $pointsAttaque;
+    private $pointsDefense;
+    private $amelioration;
+
+    public function __construct($nom, $pointsDeVie, $pointsAttaque, $pointsDefense, $amelioration){
+        $this->nom = $nom;
+        $this->pointsDeVie = $pv;
+        $this->pointsAttaque = $pa;
+        $this->pointsDefense = $pd;
+        $this->amelioration = $upgrade;
+    }
+
+}
+
+class Salle{
+    
+}
+?>
