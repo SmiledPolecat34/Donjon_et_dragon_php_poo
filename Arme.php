b670f6caf74f745f27156f8efe304166d5564b73
aboubakarkone <119500814+aboubakarkone@users.noreply.github.com>
2023-11-24 11:05:25 +0100

    Add files via upload

--- Arme.php
+++ Arme.php
@@ -49,5 +49,19 @@ class Arme{
     
 
 }
+class ArmeDAO{
+    private $bdd;
 
+    public function __construct($bdd){
+        $this->bdd = $bdd;
+    }
+
+    public function getArme($id){
+        $req = $this->bdd->prepare('SELECT * FROM armes WHERE id = :id');
+        $req->execute(array('id' => $id));
+        $donnees = $req->fetch(PDO::FETCH_ASSOC);
+        return new Arme($donnees['nom'], $donnes['rarete'], $donnees['degats'], $donnees['niveauRequis'], $donnees['poids'], $donnees['est_maudite']);
+    }
+
+}
 ?>
