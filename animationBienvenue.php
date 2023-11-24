8bf2ac5237841d4d5f80eecbc4a1c34d422fea5c
aboubakarkone <119500814+aboubakarkone@users.noreply.github.com>
2023-11-24 16:07:39 +0100

    Add files via upload

---
+++ animationBienvenue.php
@@ -0,0 +1,41 @@
+<?php
+
+function afficherProgressivement($texte, $delai = 40000) {
+    $longueur = strlen($texte);
+
+    for ($i = 0; $i < $longueur; $i++) {
+        echo substr($texte, $i, 1);
+        usleep($delai);
+    }
+}
+
+function effacerProgressivement($longueur, $delai = 40000) {
+    for ($i = $longueur - 1; $i >= 0; $i--) {
+        echo "\033[D"; 
+        echo "\033[K"; 
+        usleep($delai);
+    }
+}
+
+function afficherEtEffacer($texte, $delaiAvantEffacement = 2000000) {
+    afficherProgressivement($texte);
+    usleep($delaiAvantEffacement);
+    effacerProgressivement(strlen($texte));
+}
+
+function afficherBienvenue() {
+    echo "\033[2J"; 
+
+    $texteBienvenue = "BIENVENUE";
+    afficherEtEffacer($texteBienvenue);
+
+    $texteSuite = " DANS ";
+    afficherEtEffacer($texteSuite);
+
+    $texteSuite ="DONJON ET DRAGON";
+    afficherEtEffacer($texteSuite);
+
+    afficherEtEffacer("VOUS ALLEZ ENTRER DANS NOTRE JEU AUSSI FANTASTIQUE QUE INCROYABLE", 2000000);
+}
+
+
