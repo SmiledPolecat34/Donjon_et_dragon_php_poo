9157850953df55dabab822f381fecdaffaa87b9a
aboubakarkone <119500814+aboubakarkone@users.noreply.github.com>
2023-11-24 11:07:12 +0100

    Add files via upload

--- Enigme.php
+++ Enigme.php
@@ -103,5 +103,20 @@ class Enigme{
         }
     }
 }
+class EnigmeDAO{
+    private $bdd;
 
+    public function __construct($bdd){
+        $this->bdd = $bdd;
+    }
+
+    public function getEnigme($id){
+        $req = $this->bdd->prepare('SELECT * FROM enigmes WHERE id = :id');
+        $req->execute(array('id' => $id));
+        $donnees = $req->fetch(PDO::FETCH_ASSOC);
+        return new Enigme($donnees['question'], $donnees['reponse_correcte'], $donnees['reponse_fausse_1'], $donnees['reponse_fausse_2'], $donnees['reponse_fausse_3'], $donnees['recompense']);
+    }
+
+
+}
 ?>
