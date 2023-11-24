950e1a66408802aa637e7b0dbf17948d8da8ed52
Aykiu02 <149675695+Aykiu02@users.noreply.github.com>
2023-11-24 11:05:17 +0100

    Add files via upload

--- Personnage.php
+++ Personnage.php
@@ -126,4 +126,67 @@ class Personnage {
     
 }
 
+class PersonnageDAO{
+    protected $bdd;
+    
+    public function __construct($bdd){
+        $this->bdd = $bdd;
+    }
+    
+    public function getPersonnage($id){
+        $req = $this->bdd->prepare('SELECT * FROM personnages WHERE id = :id');
+        $req->execute(array('id' => $id));
+        $donnees = $req->fetch(PDO::FETCH_ASSOC);
+        return new Personnage($donnees['nom'], $donnees['pointsDeVie'], $donnees['pointsAttaque'], $donnees['pointsDefense'], $donnees['experience'], $donnees['niveau'], $donnees['armeEquipee'], $donnees['passif'], $donnees['type'], $donnees['est_vivant']);
+    }
+
+    public function sauvegarderPartie($personnage){
+        $req = $this->bdd->prepare('UPDATE personnages SET nom = :nom, pointsDeVie = :pointsDeVie, pointsAttaque = :pointsAttaque, pointsDefense = :pointsDefense, experience = :experience, niveau = :niveau, armeEquipee = :armeEquipee, passif = :passif, type = :type, est_vivant = :est_vivant WHERE id = :id');
+        $req->execute(array(
+            'nom' => $personnage->getNom(),
+            'pointsDeVie' => $personnage->getPointsDeVie(),
+            'pointsAttaque' => $personnage->getPointsAttaque(),
+            'pointsDefense' => $personnage->getPointsDefense(),
+            'experience' => $personnage->getExperience(),
+            'niveau' => $personnage->getNiveau(),
+            'armeEquipee' => $personnage->getArmeEquipee(),
+            'passif' => $personnage->getPassif(),
+            'type' => $personnage->getType(),
+            'est_vivant' => $personnage->getEstVivant(),
+            'id' => $personnage->getId()
+        ));
+    }
+}
+
+
+// Création des personnages
+// $aykiu = new Personnage("Aykiu", 110, 13, 8, 0, 1, $arc);
+// $franklin = new Personnage("Franklin", 120, 12 , 5, 0, 1, $batonMagique);
+// $sidick = new Personnage("Sidick", 150, 17, 10, 0, 1,$epee);
+
+// Création des armes de bases
+// $arcCommun = new Arme("Arc", 1,"Commun");
+// $batonMagiqueCommun = new Arme("Bâton Magique", 1,"Commun");
+// $epeeCommune = new Arme("Épée", 1,"Commun");
+
+
+// //épée amelioré
+// $épéeRare = new Arme("Épée longue en fer ", 25, "Rare");
+// $épéeEpique = new Arme("Lame maudite de muramasa", 40, "Epique");
+// $épéeLegendaire = new Arme("L'épée maudite du roi démon ", 40, "Légendaire");
+// $épéeMythologique = new Arme("La Tueuse de dieux", 60, "Mythologique");
+
+
+// // Arc amélioré
+// $arcRare = new Arme("Arc long en Acier ", 25, "Rare");
+// $arcEpique = new Arme("Arc des abysses  ", 40, "Epique");
+// $arcLegendaire = new Arme("Arc maudit du roi démon ", 40, "Légendaire");
+// $arcMythologique = new Arme("l'Arc tueur de dieux", 60, "Mythologique");
+
+// //Baton magique améliorer
+// $batonMagique = new Arme("baton long en Acier ", 25, "Rare");
+// $batonMagiqueEpique = new Arme("baton des abysses  ", 40, "Epique");
+// $batonMagiqueLegendaire = new Arme("baton maudit du roi démon ", 40, "Légendaire");
+// $batonMagiqueMythologique = new Arme("l'baton tueur de dieux", 60, "Mythologique");
+
 ?>
--- SalleSpeciale.php
+++ SalleSpeciale.php
@@ -18,4 +18,22 @@ class SalleSpeciale extends Salle {
     }
 }
 
+
+class SalleSpecialeDAO{
+    private $bdd;
+
+    public function __construct($bdd){
+        $this->bdd = $bdd;
+    }
+
+    public function getSalleSpeciale($id){
+        $req = $this->bdd->prepare('SELECT * FROM salles_speciales WHERE id = :id');
+        $req->execute(array('id' => $id));
+        $donnees = $req->fetch(PDO::FETCH_ASSOC);
+        return new SalleSpeciale($donnees['nom'], $donnees['type'], $donnees['description'], $donnees['specialite']);
+    }
+
+}
+
+
 ?>
