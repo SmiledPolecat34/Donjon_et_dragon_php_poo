a0049df29fda61fd0c039fe19176a0e034ecf9c3
Aykiu02 <149675695+Aykiu02@users.noreply.github.com>
2023-11-24 11:10:20 +0100

    Add files via upload

--- Inventaire.php
+++ Inventaire.php
@@ -44,4 +44,20 @@ class Inventaire{
         $this->poids_arme = $arme->getPoids();
     }
 }
+
+class InvenraireDAO{
+    private $bdd;
+
+    public function __construct($bdd){
+        $this->bdd = $bdd;
+    }
+
+    public function getInventaire($id){
+        $req = $this->bdd->prepare('SELECT * FROM inventaire WHERE id = :id');
+        $req->execute(array('id' => $id));
+        $donnees = $req->fetch(PDO::FETCH_ASSOC);
+        return new Inventaire($donnees['poids_arme'], $donnees['capacite_stockage']);
+    }
+
+}
 ?>
